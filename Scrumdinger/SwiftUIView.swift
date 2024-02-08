40078a05cd30adf8aa441996ef3f938c51ce4483
yunior123 <yuniorrodriguezo460@gmail.com>
2024-02-08 05:37:12 -0500

    df

---
+++ Scrumdinger/SwiftUIView.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftUIView.swift
+//  Scrumdinger
+//
+//  Created by Yunior Rodriguez Osorio on 8/2/24.
+//
+
+
+import SwiftUI
+
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+        }
+    }
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
